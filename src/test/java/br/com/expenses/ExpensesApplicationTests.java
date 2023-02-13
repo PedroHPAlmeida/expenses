5ae6af41633aa6a2bcae3b7bd711623affb517e6
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-02-12 21:46:33 -0300

    feat: projeto inicial

---
+++ src/test/java/br/com/expenses/ExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.expenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
