882fc36fdd6369cbd61a580e0bf952b2a56af3de
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-02-18 22:08:31 -0300

    style: removendo código de debug

--- src/main/java/br/com/expenses/controller/ExpenseController.java
+++ src/main/java/br/com/expenses/controller/ExpenseController.java
@@ -3,14 +3,11 @@ package br.com.expenses.controller;
 import br.com.expenses.model.Expense;
 import br.com.expenses.repository.ExpenseRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
 
 @AllArgsConstructor
 @Controller
@@ -21,20 +18,13 @@ public class ExpenseController {
 
     @GetMapping(path = "/form")
     public ModelAndView newExpense(Expense expense) {
-        System.out.println(expense);
         return new ModelAndView("new-expense");
     }
 
     @PostMapping(path = "new")
     public ModelAndView save(Expense expense) {
-        System.out.println(expense);
         expenseRepository.save(expense);
         return new ModelAndView("new-expense");
-    }
-
-    @GetMapping(path = "/list")
-    private List<Expense> list() {
-        return expenseRepository.findAll();
     }
 
 }
