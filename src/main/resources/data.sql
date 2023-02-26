ec5b0c52c54bcc6e2d535a2978b48aa6a57e80d5
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-02-26 00:30:44 -0300

    chore: adicionando arquivo para popular o banco de dados para testes locais

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO financial_record (id, title, amount, date, current_month, description, transaction_type)
+    VALUES
+        (1, null, 1000.0, '2023-03-01', 'março', null, 'SALARIO'),
+        (2, 'Luz', 60.0, '2023-03-01', 'março', 'Pagamento da luz', 'DEBITO'),
+        (3, 'Água', 55.0, '2023-03-02', 'março', 'Pagamento da água', 'DEBITO'),
+        (4, 'Internet', 120.0, '2023-03-03', 'março', 'Pagamento da internet', 'DEBITO'),
+        (5, null, 600.0, '2023-03-04', 'março', null, 'VALE'),
+        (6, 'Mercado', 300.0, '2023-03-05', 'março', 'Comida', 'DEBITO'),
+        (7, 'Cabeleireiro', 80.0, '2023-03-06', 'março', 'Mensalidade', 'DEBITO'),
+        (8, 'Celular', 30.0, '2023-03-07', 'março', 'Crédito', 'DEBITO'),
+        (9, 'Passagem', 40.0, '2023-03-08', 'março', 'Passagem de trem', 'DEBITO'),
+        (10, 'Teclado', 200.0, '2023-03-09', 'março', 'Teclado mecânico Mercado Livre', 'DEBITO');
