8eb5d6f117dcd43541e26056ef15bd5aafcc3279
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-02-25 23:30:09 -0300

    feat: adicionando endpoint da API para buscar registros por tipo

--- src/main/java/br/com/expenses/api/FinancialRecordApi.java
+++ src/main/java/br/com/expenses/api/FinancialRecordApi.java
@@ -1,10 +1,12 @@
 package br.com.expenses.api;
 
 import br.com.expenses.model.FinancialRecord;
+import br.com.expenses.model.TransactionType;
 import br.com.expenses.repository.FinancialRecordRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,6 +21,12 @@ public class FinancialRecordApi {
     @GetMapping
     public List<FinancialRecord> findAll() {
         return financialRecordRepository.findAll();
+    }
+
+    @GetMapping(path = "/filter")
+    public List<FinancialRecord> findByTransactionType(@RequestParam(value = "transactionType") String transactionType) {
+        TransactionType transaction = TransactionType.valueOf(transactionType.toUpperCase());
+        return financialRecordRepository.findByTransactionType(transaction);
     }
 
 }
--- src/main/java/br/com/expenses/repository/FinancialRecordRepository.java
+++ src/main/java/br/com/expenses/repository/FinancialRecordRepository.java
@@ -1,7 +1,11 @@
 package br.com.expenses.repository;
 
 import br.com.expenses.model.FinancialRecord;
+import br.com.expenses.model.TransactionType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FinancialRecordRepository extends JpaRepository<FinancialRecord, Long> {
+    List<FinancialRecord> findByTransactionType(TransactionType transaction);
 }
